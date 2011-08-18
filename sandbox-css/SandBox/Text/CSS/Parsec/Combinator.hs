ebedf0f8273c203f185716cb2715683068d47c14
Hiroaki Nakamura <hnakamur@gmail.com>
2011-08-18 23:03:58 +0900

    Added permutation parse functions.

--- sandbox-css/SandBox/Text/CSS/Parsec/Combinator.hs
+++ sandbox-css/SandBox/Text/CSS/Parsec/Combinator.hs
@@ -1,13 +1,48 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module SandBox.Text.CSS.Parsec.Combinator
-    ( countRange
+    ( allInAnyOrder
+    , oneOrMoreInAnyOrder
+    , countRange
     , countMax
     , countMin
     , manyUpTo
     ) where
 
 import Text.Parsec ((<|>), Stream, ParsecT, many, try)
+
+allInAnyOrder :: Stream s m t =>
+                       [ParsecT s u m a] -> ParsecT s u m [a]
+allInAnyOrder ps = walk ps []
+    where
+        walk :: Stream s m t => [ParsecT s u m a] -> [ParsecT s u m a] ->
+                                ParsecT s u m [a]
+        walk [] [] = return []
+        walk [p] qs =
+            p >>= \r -> walk (reverse qs) [] >>= \s -> return (r:s)
+        walk (p:ps) qs =
+            (p >>= \r -> walk ((reverse qs)++ps) [] >>= \s -> return (r:s))
+            <|>
+            (walk ps (p:qs))
+
+oneOrMoreInAnyOrder :: Stream s m t =>
+                       [ParsecT s u m a] -> ParsecT s u m [a]
+oneOrMoreInAnyOrder ps = walk ps [] []
+    where
+        walk :: Stream s m t => [ParsecT s u m a] -> [ParsecT s u m a] ->
+                                [a] -> ParsecT s u m [a]
+        walk [] [] rs = return (reverse rs)
+        walk [p] qs rs =
+            p >>= \r -> (walk (reverse qs) [] (r:rs)
+                         <|> return (reverse (r:rs))
+                        )
+        walk (p:ps) qs rs =
+            (p >>= \r -> (walk ((reverse qs)++ps) [] (r:rs)
+                          <|> return (reverse (r:rs))
+                         )
+            )
+            <|>
+            (walk ps (p:qs) rs)
 
 countRange :: Stream s m t =>
               Int -> Int -> ParsecT s u m a -> ParsecT s u m [a]
